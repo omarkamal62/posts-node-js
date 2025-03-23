949830e5db792abeaca17efcacc008de91b61468
omarkamal62 <okamal34@gmail.com>
2025-03-23 16:12:34 +0200

    adding cors

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ require("dotenv").config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Options, Post, Get, Put, Patch, Delete"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(process.env.PORT);
