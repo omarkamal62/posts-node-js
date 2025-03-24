fcb5f6f4ed35078c691a7f84f094a863d82282d3
omarkamal62 <okamal34@gmail.com>
2025-03-24 05:12:43 +0200

    adding get post and get posts

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -3,20 +3,17 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post",
-        imageUrl: "/images/1.jpg",
-        creator: {
-          name: "Omar",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.json({
+        message: "Posts fetched successfully",
+        posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -46,6 +43,26 @@ exports.createPost = (req, res, next) => {
         message: "Post Created Successfully!",
         post: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not Found");
+        error.statusCode = 404;
+
+        throw error;
+      }
+
+      res.status(200).json({ message: "Post Fetched Successfully", post });
     })
     .catch((err) => {
       if (!err.statusCode) err.statusCode = 500;
