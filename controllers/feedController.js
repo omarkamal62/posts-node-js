a56cab855d580e6a928ff679f02e400798117f30
omarkamal62 <okamal34@gmail.com>
2025-03-24 16:00:44 +0200

    adding delete post

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -134,6 +134,31 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not Found");
+        error.statusCode = 404;
+
+        throw error;
+      }
+
+      clearImage(post.imageUrl);
+      return Post.deleteOne({ _id: post._id });
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted successfully" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
+};
+
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, "..", filePath);
   fs.unlink(filePath, (err) => console.log(err));
