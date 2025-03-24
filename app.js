5f44f7c0f3a07ee6939f150734881f0599b17558
omarkamal62 <okamal34@gmail.com>
2025-03-24 20:17:32 +0200

    adding the authorization

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
   );
 
   next();
