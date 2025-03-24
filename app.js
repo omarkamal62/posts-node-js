05ced3e9b8afc2043c407ae7a91b83fd512ae8f5
omarkamal62 <okamal34@gmail.com>
2025-03-24 04:58:05 +0200

    adding the static and the error handling

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +22,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+
+  res.status(status).json({
+    message,
+  });
+});
 
 mongoose
   .connect(process.env.DB_URL)
--- controllers/feedController.js
+++ controllers/feedController.js
@@ -23,9 +23,10 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Validation Failed",
-    });
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+
+    throw error;
   }
 
   const title = req.body.title;
@@ -46,5 +47,8 @@ exports.createPost = (req, res, next) => {
         post: result,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
 };
