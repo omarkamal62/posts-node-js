dc96856937a89ffc554bb34caf23a7a0d67efcab
omarkamal62 <okamal34@gmail.com>
2025-03-24 18:17:25 +0200

    adding login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -28,6 +29,49 @@ exports.signup = (req, res, next) => {
       res.status(201).json({
         message: "User created!",
         userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with email cannot be found");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isMatched) => {
+      if (!isMatched) {
+        const error = new Error("Wrong password");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        process.env.SECRET,
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({
+        token,
+        userId: loadedUser._id.toString(),
       });
     })
     .catch((err) => {
