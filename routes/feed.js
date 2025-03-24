5f9b16b469c6d2cbb27450af0c656891ffb0af9d
omarkamal62 <okamal34@gmail.com>
2025-03-24 02:28:23 +0200

    adding the validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
@@ -6,6 +7,13 @@ const feedController = require("../controllers/feedController");
 
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
