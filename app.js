f4df44a82514028e3cf29a7d07f03c36ccb66f9c
omarkamal62 <okamal34@gmail.com>
2025-03-24 06:34:30 +0200

    updating the post

--- app.js
+++ app.js
@@ -35,11 +35,15 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "Options, Post, Get, Put, Patch, Delete"
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
 
   next();
 });
