ba4e06db348e85cb4e1ed9f9511667199cdc12ec
omarkamal62 <okamal34@gmail.com>
2025-03-25 02:44:34 +0200

    handling the update and get of the user status

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const { body } = require("express-validator");
 const User = require("../models/user");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -26,5 +27,14 @@ router.put(
 );
 
 router.post("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
